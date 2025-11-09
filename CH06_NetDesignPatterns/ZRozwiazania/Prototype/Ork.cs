07890eec7233dcdbec58ea2cbf1b02e793393d09
MsTrue <ynatalia.kowalska@gmail.com>
2025-11-09 17:24:56 +0100

    prototype

---
+++ CH06_NetDesignPatterns/ZRozwiazania/Prototype/Ork.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+
+namespace RefactoringToDesignPatterns.CH06_NetDesignPatterns.ZRozwiazania.Prototype
+{
+    public class Ork
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Strength { get; set; }
+        public int Agility { get; set; }
+
+
+
+        public Ork Clone()
+        {
+            var json = JsonConvert.SerializeObject(this);
+
+            return JsonConvert.DeserializeObject<Ork>(json)!;
+        }
+
+        public static void ListOrks(IEnumerable<Ork> orks)
+        {
+           
+            foreach (var ork in orks)
+            {
+                Console.WriteLine($"{ork.Name}, STR={ork.Strength}");
+            }
+        }
+    }
+}
---
+++ CH06_NetDesignPatterns/ZRozwiazania/Prototype/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactoringToDesignPatterns.CH06_NetDesignPatterns.ZRozwiazania.Prototype
+{
+     class Program
+    {
+        static void Main()
+        {
+            var baseOrk = new Ork
+            {
+                Id = Guid.NewGuid(),
+                Name = "Name1",
+                Strength = 10,
+                Agility = 10
+            };
+
+            var rnd = new Random();
+            var orcs = new List<Ork>();
+
+            for (int i = 1; i <+ 8; i++)
+            {
+                var clone = baseOrk.Clone();
+
+                clone.Id = Guid.NewGuid();
+                clone.Name = $"{baseOrk.Name.Substring(0, 3)} + {i}";
+
+                int randomStrength = rnd.Next(1, 25);
+                clone.Strength = randomStrength;
+
+
+                orcs.Add(clone);
+            }
+
+            Ork.ListOrks(orcs);
+        }
+        }
+    }
